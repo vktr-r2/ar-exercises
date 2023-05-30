f1b48d706e20385e8a43c2765ecdc955b4b3a485
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-30 14:09:57 -0400

    Complete excersize_7 requirements

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,13 @@
 class Employee < ActiveRecord::Base
+
+  # establish relationship to store table
   belongs_to :store
+
+  # define validations to be checked
+  validates_associated :store             #store is assoicated with employee?
+  validates :first_name, presence: true   #first name?
+  validates :last_name, presence: true    #last name?
+  validates :hourly_rate,                 # hourly rate is a number between 40 and 200?
+    presence: true, 
+    numericality: { only_integer: true, greater_than: 40, less_than: 200 }
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,20 @@
 class Store < ActiveRecord::Base
+
+  # estalbish relationship to employees table 
   has_many :employees
+
+  # define validations to be checked
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue,
+    presence: true,
+    numericality: { only_integer: true, greater_than: 0}
+    validate :carry_apparel
+
+  # define custom validator
+  def carry_apparel
+    if !(mens_apparel || womens_apparel)
+      errors.add(:mens_apparel, "cannot be false if womens_apparel is also false")
+      errors.add(:womens_apparel, "cannot be false if mens_apparel is also false")
+    end
+  end
 end
